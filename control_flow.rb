b4d108ae52548e112ea26daf465aa32d801b06e0
Michael Franco <franco.luis.michael@gmail.com>
2022-04-27 12:45:20 -0500

    commit tree 303283b238209c04dede8880896923130f9001fa

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,24 @@
 def admin_login(username, password)
-  # your code here
+  password = "12345"
+  if username == "admin"
+    "Access granted"
+  elsif username == "ADMIN"
+    "Access granted"
+  else password == "12345" ? "Access granted" : "Access denied"
+  
 end
 
-def hows_the_weather(temperature)
-  # your code here
-end
+# def hows_the_weather(temperature)
+#   if temperature < 40
+#     "It's brisk out there!"
+#   end
+# end
 
-def fizzbuzz(num)
-  # your code here
-end
+# def fizzbuzz(num)
+#   # your code here
+# end
 
-def calculator(operation, num1, num2)
-  # your code here
-end
+# def calculator(operation, num1, num2)
+#   # your code here
+# end
 
